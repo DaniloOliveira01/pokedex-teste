1793992e6e19d2c23d5997ef11163050ad3aaa4f
Danilo Oliveira <oliveira.83555@gmail.com>
2022-04-20 21:39:46 -0300

    feat: config import components

---
+++ src/components/config-imports.tsx
@@ -0,0 +1,4 @@
+export { default as Header } from './Header'
+export { default as LogoPoke } from './LogoPoke'
+export { default as Menu } from './Menu'
+export { default as SearchPoke } from './SearchPoke'
