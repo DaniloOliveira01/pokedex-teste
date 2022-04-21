d81a14d9919de96306cd2aac526c4341c6e2dabe
Danilo Oliveira <oliveira.83555@gmail.com>
2022-04-20 21:49:14 -0300

    querie api graphql pokemon ok!

---
+++ src/querys/pokemons.ts
@@ -0,0 +1,42 @@
+import {gql} from '@apollo/client'
+
+export const GET_POKEMONS = gql`
+  query pokemons($limit: Int, $offset: Int) {
+    pokemons(limit: $limit, offset: $offset) {
+      results {
+        id
+        name
+      }
+    }
+  }
+`;
+export const gqlVariables = {
+  limit: 150,
+  offset: 0,
+};
+
+export const GET_POKEMON_BY_NAME = gql`
+query getPokemonByName($name: String!){
+  pokemon(name: $name){
+    name
+    id
+    sprites {
+      front_default
+      back_default
+    }
+    height
+    weight
+    types{
+      type{
+        name
+      }
+    }
+    stats{
+      base_stat
+      stat{
+        name
+      }
+    }
+  }
+}
+`
