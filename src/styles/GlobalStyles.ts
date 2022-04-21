3ead4614bfe163bc30b0562e51cd1cc28a382863
Danilo Oliveira <oliveira.83555@gmail.com>
2022-04-20 21:45:02 -0300

    feat: add and config themes dark/light e global style

---
+++ src/styles/GlobalStyles.ts
@@ -0,0 +1,51 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+html {
+  scroll-behavior: smooth;
+}
+
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
+body {
+  background-color: #fff;
+  color: #000 ;
+
+  font-family: 'Roboto', sans-serif;
+  margin: 0;
+  padding: 0;
+}
+
+a {
+  color: #f9f9f9;
+  text-decoration: none;
+}
+
+@media only screen and (min-width: 768px) {
+  body {
+    font-size: 16px;
+  }
+}
+
+@media only screen and (min-width: 480px) and (max-width: 768px) {
+  body {
+    font-size: 15px;
+  }
+}
+
+@media only screen and (min-width: 768px) {
+  body {
+    font-size: 16px;
+  }
+}
+
+@media only screen and (max-width: 479px) {
+  body {
+    font-size: 14px;
+  }
+} 
+`;
---
+++ src/styles/dark-theme.ts
@@ -0,0 +1,4 @@
+export const darkTheme = {
+  primaryBg: '#1C1C1C',
+  secundaryBg: '#2F4F4F'
+};
