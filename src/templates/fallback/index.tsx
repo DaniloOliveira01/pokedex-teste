9f3a89f2c64fd88319a324d75f08eae6e41e1518
Danilo Oliveira <oliveira.83555@gmail.com>
2022-04-24 22:52:22 -0300

    chore: add loading for fallback

---
+++ src/templates/fallback/index.tsx
@@ -0,0 +1,11 @@
+import * as S from "./styles";
+
+export const Fallback = () => {
+  return (
+    <S.Container>
+      <S.Content>
+        <h3>Carregando...</h3>
+      </S.Content>
+    </S.Container>
+  );
+}
---
+++ src/templates/fallback/styles.ts
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background-color: #C0C0C0;
+  backdrop-filter: blur(4px);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Content = styled.div`
+  line-height: 1.3;
+   
+  h3 {
+    font-weight: 500;
+  }
+`;
