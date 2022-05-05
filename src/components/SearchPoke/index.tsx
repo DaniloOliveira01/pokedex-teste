325c0703ffe97aec8a834d120b3824f7c9fd4f74
Danilo Oliveira <dan.oliveira883@gmail.com>
2022-05-05 19:29:57 -0300

    feat: add search dinamic

--- src/components/SearchPoke/index.tsx
+++ src/components/SearchPoke/index.tsx
@@ -1,11 +1,14 @@
 import * as S from "./styles";
 import { FaSearch } from 'react-icons/fa'
+import { ISearchProps } from "@/@types/type-components";
 
-export const SearchPoke = () => {
+export const SearchPoke = ({ search, setSearch }: ISearchProps) => {
   return (
     <S.SearchInput>
       <S.InputCustom 
         placeholder="Search by name or number"
+        value={search}
+        onChange={({ target }) => setSearch(target.value)}
       />
       <FaSearch />
     </S.SearchInput>
